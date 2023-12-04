6327015534d822257e1e736db87463c4c38a04b5
Harlen Alvarez <harlenalvarez@gmail.com>
2023-12-04 15:05:43 -0500

    feat: change from cb to events dispatch

--- src/lib/hooks/useRedrawEvent.tsx
+++ src/lib/hooks/useRedrawEvent.tsx
@@ -4,27 +4,29 @@ import { useEffect, useRef } from 'react';
 import { canvasTransform, getCanvas2DContext } from '..';
 
 
-const clearBeforeRedraw = (cb: () => Promise<any> | any) => () => {
+const clearBeforeRedraw = (cb: () => Promise<unknown> | unknown) => () => {
   const ctx = getCanvas2DContext();
   if (!ctx) return;
   clearCanvas(ctx, canvasTransform.scale * devicePixelRatio, canvasTransform.offset);
   cb();
 }
 
+const customEvent = new CustomEvent('request-redraw');
+
 
 const registerEvent = (ev: ReturnType<typeof clearBeforeRedraw>) => {
-  drawSubscribers.add(ev);
+  document.addEventListener('request-redraw', ev)
   return () => {
-    drawSubscribers.delete(ev);
+    document.removeEventListener('request-redraw', ev)
   }
 }
 
 export const requestRedraw = () => {
-  for (const sub of drawSubscribers) sub();
+  document.dispatchEvent(customEvent);
 }
 
-export const useRedrawEvent = (cb: () => Promise<any> | any, deps: React.DependencyList) => {
-  let evUnsub = useRef<() => void>();
+export const useRedrawEvent = (cb: () => Promise<unknown> | unknown, deps: React.DependencyList) => {
+  const evUnsub = useRef<() => void>();
   useEffect(() => {
     evUnsub.current = registerEvent(clearBeforeRedraw(cb));
     return () => {
