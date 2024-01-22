3180e08e296d7b5c7efb8cf295a8511325af143c
Harlen Alvarez <harlenalvarez@gmail.com>
2024-01-22 15:45:45 -0500

    fix: get fab context by ref

--- src/lib/container/ClickAwayListener.tsx
+++ src/lib/container/ClickAwayListener.tsx
@@ -1,6 +1,6 @@
 import { getCanvasPoint } from '@practicaljs/canvas-kit';
 import { ReactNode, useCallback, useEffect, useRef } from 'react';
-import { getCanvas2DContext, useFabContextSelect } from '..';
+import { getCanvas2DContext, getFabContext, useFabContextSelect } from '..';
 
 type ClickAwayListenerProps = {
     children: ReactNode;
@@ -10,18 +10,18 @@ type ClickAwayListenerProps = {
 
 export const ClickAwayListener = ({ children, fabId, onClickAway }: ClickAwayListenerProps) => {
     const wrapperRef = useRef<HTMLDivElement>(null);
-    const path = useFabContextSelect(fabId, 'path');
 
     const handleClickOutside = useCallback((event: MouseEvent) => {
         const ctx = getCanvas2DContext()
         if(!ctx) return;
         const clickedOutside = wrapperRef.current && !wrapperRef.current.contains(event.target as Node)
         const [canvasX, canvasY] = getCanvasPoint(event.offsetX, event.offsetY, ctx, true)
-        const clickedOnNode = !path ? false :  ctx.isPointInPath(path, canvasX, canvasY)
+        const fab = getFabContext(fabId)
+        const clickedOnNode = !fab.path ? false :  ctx.isPointInPath(fab.path, canvasX, canvasY)
         if(clickedOutside && !clickedOnNode)
           onClickAway();
         
-    }, [onClickAway, path]);
+    }, [onClickAway, fabId]);
 
     useEffect(() => {
         // Bind the event listener
